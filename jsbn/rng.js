073d0f680464222ce626c55cc48ce19c2a13224d
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-21 13:05:23 -0400

    stop requiring "navigator"

--- jsbn/rng.js
+++ jsbn/rng.js
@@ -27,12 +27,14 @@ if(rng_pool == null) {
   rng_pool = new Array();
   rng_pptr = 0;
   var t;
-  if(navigator.appName == "Netscape" && navigator.appVersion < "5" && window.crypto) {
-    // Extract entropy (256 bits) from NS4 RNG if available
-    var z = window.crypto.random(32);
-    for(t = 0; t < z.length; ++t)
-      rng_pool[rng_pptr++] = z.charCodeAt(t) & 255;
-  }  
+  if (typeof navigator !== "undefined" && navigator !== null) {
+    if(navigator.appName == "Netscape" && navigator.appVersion < "5" && window.crypto) {
+      // Extract entropy (256 bits) from NS4 RNG if available
+      var z = window.crypto.random(32);
+      for(t = 0; t < z.length; ++t)
+        rng_pool[rng_pptr++] = z.charCodeAt(t) & 255;
+    }
+  }
   while(rng_pptr < rng_psize) {  // extract some randomness from Math.random()
     t = Math.floor(65536 * Math.random());
     rng_pool[rng_pptr++] = t >>> 8;
